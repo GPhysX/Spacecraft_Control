9658a5007f8e034ef6b7969861d5c58cffd7ab50
krijnen <hielke.krijnen@hccnet.nl>
2016-03-23 22:15:13 +0100

    initial codde

--- spacecraft.py
+++ spacecraft.py
@@ -1,1 +1,14 @@
 #
+
+class Spacecraft(object):
+
+
+	def __init__(self, ixx = 124.531, iyy = 124.586, izz = 0.704, orbit = 700000, orientation=orientation(), ang_velocity = ang_velocity(), ang_accel = ang_accel() ):
+		self.ixx = ixx
+		self.iyy = iyy
+		self.izz = izz
+		self.orbit = orbit 					#for now a constant, since only a circular orbit is used
+		self.J = np.array([[ixx,0,0],[0,iyy,0],[0,0,izz]])
+		self.orientation = orientation
+		self.ang_accel = ang_accel
+		self.ang_velocity = ang_velocity
