9aa17c296f4017ef00f3512d5feb8d388e5b8d3b
krijnen <hielke.krijnen@hccnet.nl>
2016-04-06 20:57:45 +0200

    yup

--- mainloop.py
+++ mainloop.py
@@ -1,3 +1,18 @@
+from math import *
+import numpy as numpy
+from spacecraft import *
+
+
 def main():
+	sc = spacecraft(euler())
+
+	for i in range(0,100):
+		sc.Heun()
+		print (sc.euler.rot)
 
 
+
+
+
+if __name__ == '__main__':
+	main()
--- spacecraft.py
+++ spacecraft.py
@@ -1,28 +1,39 @@
 #
 import numpy as np
 from math import *
+delta_t = 0.001
 
-class Spacecraft(object):
+class spacecraft(object):
 
-	def __init__(self, ixx = 124.531, iyy = 124.586, izz = 0.704, orbit = 700000, orientation=orientation(), euler = euler()):
+	def __init__(self, euler, ixx = 124.531, iyy = 124.586, izz = 0.704, orbit = 700000):
 		self.ixx = ixx
 		self.iyy = iyy
 		self.izz = izz
 		self.orbit = orbit 					#for now a constant, since only a circular orbit is used
-		self.J = np.array([[ixx,0,0],[0,iyy,0],[0,0,izz]])
-
-		self.orientation = orientation
-		self.ang_accel = ang_accel
-		self.ang_velocity = ang_velocity
+		self.J = np.matrix([[ixx,0,0],[0,iyy,0],[0,0,izz]])
 		self.euler = euler
+		history = [self.euler]
 
-
-
-	def Kinematic_euler(self, u):
-		fx = np.matrix([self.euler.N * self.euler.rot],[np.cross(-self.J.getI()*self.euler.OMEGA, self.J*self.euler.rot])
-		Gx = np.matrix([np.zeros(3,3), np.zeros(3,3)],[self.J.getI(), self.J.getI()])
+	def Kinematic_euler(self, u, rot = 0):
+		if rot == 0:
+			rot = self.euler.rot					
+		fx1 = self.euler.N * rot
+		fx2 = np.cross(-self.J.I*self.euler.OMEGA_fn(rot), self.J*rot, axis = 0)
+		fx = np.array([[fx1], [fx2]])
+				#fx = np.matrix([[self.euler.N * rot],[np.cross(-self.J.I*self.euler.OMEGA_fn(rot), self.J*rot, axis = 0)]])
+		print fx
+		Gx = np.matrix([[np.zeros(3), np.zeros(3)],[self.J.I, self.J.I]])
 		x_d = fx + Gx * u
-		
+		print x_d
+		return x_d
+
+	def Heun(self, u = np.matrix([0.01,0.01,0.01])):
+		x_d = self.Kinematic_euler(u)
+		rot_ = self.euler.rot + x_d * delta_t
+		x_d2 = self.Kinematic_euler(u, rot_)
+		rot = self.euler.rot + delta_t/2*(x_d + x_d2)
+		self.euler.update_rot(rot)
+		self.history.append(self.euler)
 
 
 
@@ -42,28 +53,39 @@ class euler(object):
 		self.theta2_d = theta2_d
 		self.theta3 = theta3
 		self.theta3_d = theta3_d
-		self.attitude = np.array([theta1, theta2, theta3])
-		self.rot = np.array([theta1_d, theta2_d, theta3_d])
-		self.state = np.array([self.attitude, self.rot])
-		self.OMEGA = np.array([0, -theta3_d, theta2_d],[theta3_d, 0, -theta1_d],[-theta2_d, theta1_d, 0])
-		self.N = np.array([1, sin(theta1)*tan(theta2), cos(theta1)*tan(theta2)],[0, cos(theta1), -sin(theta1)],[0, sin(theta1)/cos(theta2), cos(theta1)/cos(theta2)])
+		self.attitude = np.matrix([theta1, theta2, theta3]).T
+		self.rot = np.matrix([theta1_d, theta2_d, theta3_d]).T
+		self.state = np.array([self.attitude.T, self.rot.T]).T
+		self.OMEGA = np.matrix([[0, -theta3_d, theta2_d],[theta3_d, 0, -theta1_d],[-theta2_d, theta1_d, 0]])
+		self.N = np.matrix([[1, sin(theta1)*tan(theta2), cos(theta1)*tan(theta2)],[0, cos(theta1), -sin(theta1)],[0, sin(theta1)/cos(theta2), cos(theta1)/cos(theta2)]])
 
-	def update_attitude(theta1 = self.theta1, theta2 = self.theta2, theta3 = self.theta3):
+	def update_attitude(theta1, theta2, theta3):
 		self.theta1 = theta1
 		self.theta2 = theta2
 		self.theta3 = theta3
-		self.attitude = np.array([theta1, theta2, theta3])
-		self.N = np.array([1, sin(theta1)*tan(theta2), cos(theta1)*tan(theta2)],[0, cos(theta1), -sin(theta1)],[0, sin(theta1)/cos(theta2), cos(theta1)/cos(theta2)])
+		self.attitude = np.matrix([theta1, theta2, theta3]).T
+		self.N = np.matrix([[1, sin(theta1)*tan(theta2), cos(theta1)*tan(theta2)],[0, cos(theta1), -sin(theta1)],[0, sin(theta1)/cos(theta2), cos(theta1)/cos(theta2)]])
 
 
-	def update_rot(theta1_d = self.theta1_d, theta2_d = self.theta2_d, theta3 = self.theta3_d):
+	def update_rot(theta1_d, theta2_d, theta3):
 		self.theta1_d = theta1_d
 		self.theta2_d = theta2_d
 		self.theta3_d = theta3_d
-		self.rot = np.array([theta1_d, theta2_d, theta3_d])
-		self.state = np.array([self.attitude, self.rot])
-		self.OMEGA = np.array([0, -theta3_d, theta2_d],[theta3_d, 0, -theta1_d],[-theta2_d, theta1_d, 0])
+		self.rot = np.matrix([theta1_d, theta2_d, theta3_d]).T
+		self.state = np.array([self.attitude.T, self.rot.T]).T
+		self.OMEGA = np.matrix([[0, -theta3_d, theta2_d],[theta3_d, 0, -theta1_d],[-theta2_d, theta1_d, 0]])
 		
+	def update_var(rot, att = 0):
+		if not att == 0:
+			self.theta1 = att[0]
+			self.theta2 = att[1]
+			self.theta3 = att[2]
 
-		
-		
+		self.theta1_d = rot[0]
+		self.theta2_d = rot[1]
+		self.theta3_d = rot[2]
+
+	def OMEGA_fn(self, rot):
+		return np.matrix([[0, -rot[2], rot[1]],[rot[2], 0, -rot[0]],[-rot[1], rot[0], 0]])
+
+
